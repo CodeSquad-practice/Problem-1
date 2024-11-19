8220fc8544344b3ded1bcc682ad40e7e385edcd4
2jiyong <jdragon2000@naver.com>
2024-11-19 15:55:21 +0900

    Update random arr and input function
    I updated random arr function by Fisher-Yates algorithm to reduce time
    complexity. And add comment in input_nums function. Also updated to
    remove leading spaces from num2 and recheck.

--- problem-1.py
+++ problem-1.py
@@ -3,13 +3,12 @@ import random
 #무작위 배열 생성
 def make_random_arr(start,end):
     arr=[i for i in range(start,end+1)]
-    rand_arr=[]
-    while arr:
-        chosen_idx=random.randrange(0,len(arr))
-        rand_arr.append(arr[chosen_idx])
-        arr.pop(chosen_idx)
+    #Fisher-Yates 알고리즘 구현
+    for i in range(len(arr)-1,-1,-1):
+        idx=random.randint(0,i)
+        arr[i],arr[idx]=arr[idx],arr[i]
 
-    return rand_arr
+    return arr
 
 def main():
     arr=make_random_arr(1,8)
@@ -34,15 +33,24 @@ def input_nums():
         nums=list(input().split(","))
         #2개가 아니면 다시
         if len(nums)!=2:
+            print("\n잘못 입력하셨습니다. 다시 입력해주세요\n")
             continue
 
         num1,num2=nums[0],nums[1]
-        
+        #num2의 시작이 공백이면 공백제거
+        if num2[0]==" ":
+            num2=num2[1:]
+        #2개 모두 숫자인가?
         if not (num1.isdigit() and num2.isdigit()):
+            print("\n잘못 입력하셨습니다. 다시 입력해주세요\n")
             continue
+        # 2개 모두 범위 안인가?
         elif not (in_range(int(num1)) and in_range(int(num2))):
+            print("\n잘못 입력하셨습니다. 다시 입력해주세요\n")
             continue
+        # 2 숫자가 같은가?
         elif int(num1)==int(num2):
+            print("\n잘못 입력하셨습니다. 다시 입력해주세요\n")
             continue
         
         return  int(num1),int(num2)
